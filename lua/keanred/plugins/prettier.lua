8a195bb2a96fadc602d0a1dcf93fdfb357a3b119
Keanred <47241653+Keanred@users.noreply.github.com>
2026-02-26 22:07:18 +0200

    Add prettier config

---
+++ lua/keanred/plugins/prettier.lua
@@ -0,0 +1,41 @@
+return {
+   {
+        "sbdchd/neoformat",
+        config = function()
+          vim.g.neoformat_try_node_exe = 1
+          vim.g.neoformat_only_msg_on_error = 1
+          vim.g.neoformat_enabled_javascript = { "prettier" }
+          vim.g.neoformat_enabled_javascriptreact = { "prettier" }
+          vim.g.neoformat_enabled_typescript = { "prettier" }
+          vim.g.neoformat_enabled_typescriptreact = { "prettier" }
+          vim.g.neoformat_enabled_json = { "prettier" }
+          vim.g.neoformat_enabled_css = { "prettier" }
+          vim.g.neoformat_enabled_scss = { "prettier" }
+          vim.g.neoformat_enabled_markdown = { "prettier" }
+          vim.g.neoformat_enabled_yaml = { "prettier" }
+          vim.g.neoformat_enabled_html = { "prettier" }
+
+          local augroup = vim.api.nvim_create_augroup
+          local autocmd = vim.api.nvim_create_autocmd
+          local neoformat_group = augroup("Neoformat", { clear = true })
+
+          autocmd("BufWritePre", {
+            group = neoformat_group,
+            pattern = {
+              "*.js",
+              "*.jsx",
+              "*.ts",
+              "*.tsx",
+              "*.json",
+              "*.css",
+              "*.scss",
+              "*.md",
+              "*.yaml",
+              "*.yml",
+              "*.html",
+            },
+            command = "Neoformat",
+          })
+        end,
+    },
+}
