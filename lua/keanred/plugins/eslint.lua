5e2b746d7178838136ccc92b74006f2f2af4f2c8
Keanred <47241653+Keanred@users.noreply.github.com>
2026-03-07 19:47:16 +0200

    modify eslint config

--- lua/keanred/plugins/eslint.lua
+++ lua/keanred/plugins/eslint.lua
@@ -10,6 +10,20 @@ return {
           run = "onSave",
         },
       })
+
+      local augroup = vim.api.nvim_create_augroup
+      local autocmd = vim.api.nvim_create_autocmd
+      local eslint_format_group = augroup("EslintFormat", { clear = true })
+
+      autocmd("BufWritePre", {
+        group = eslint_format_group,
+        pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
+        callback = function()
+          vim.lsp.buf.format({
+            async = false,
+          })
+        end,
+      })
     end,
     }
 }
