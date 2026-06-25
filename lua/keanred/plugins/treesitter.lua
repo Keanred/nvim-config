02f6804a5acec12cfe84a876357f7985067f08d1
Keanred <47241653+Keanred@users.noreply.github.com>
2026-06-25 21:51:36 +0300

    feat: add claudecode

--- lua/keanred/plugins/treesitter.lua
+++ lua/keanred/plugins/treesitter.lua
@@ -1,9 +1,12 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     lazy = false,
     build = ":TSUpdate",
     config = function()
+        local treesitter = require("nvim-treesitter")
         local treesitter_parser_config = require("nvim-treesitter.parsers")
+
         treesitter_parser_config.templ = {
             install_info = {
                 url = "https://github.com/vrischmann/tree-sitter-templ.git",
@@ -14,5 +17,44 @@ return {
         }
 
         vim.treesitter.language.register("templ", "templ")
+
+        treesitter.setup({
+            install_dir = vim.fn.stdpath("data") .. "/site",
+            auto_install = true,
+            ensure_installed = {
+                "bash",
+                "c",
+                "css",
+                "go",
+                "gomod",
+                "gosum",
+                "gowork",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "luadoc",
+                "markdown",
+                "markdown_inline",
+                "query",
+                "regex",
+                "templ",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+        })
+
+        local ts_features_group = vim.api.nvim_create_augroup("keanred_treesitter_features", { clear = true })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = ts_features_group,
+            callback = function(args)
+                pcall(vim.treesitter.start, args.buf)
+                vim.api.nvim_set_option_value("indentexpr", "v:lua.require'nvim-treesitter'.indentexpr()", { buf = args.buf })
+            end,
+        })
     end
 }
